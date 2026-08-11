70e007cf1187d81234ce5f784551910e3be1bcbb
David Fuhrmann <info@fuhrmannsoftwaresolution.de>
2026-08-11 12:41:50 +0200

    Save Push

--- backend/src/main/java/com/daf/backend/websocket/LogWebSocketHandler.java
+++ backend/src/main/java/com/daf/backend/websocket/LogWebSocketHandler.java
@@ -4,14 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 @Component
 @Slf4j
